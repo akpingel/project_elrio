09e54a1c61eb5ae73f43172cc528c4c95acc4c49
Alyssa Pingel <alyssa.pingel@my.simpson.edu>
2018-10-23 10:29:26 -0500

    changes to about .php

--- public_html/about.php
+++ public_html/about.php
@@ -18,37 +18,8 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="index.html">El Rio</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<?php include '../resources/navigation.php'; ?>
 
-  <!--<div class="d-none d-md-block col-2"> -->
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index.html">Home </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="about.html">About<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="drinks.html">Drinks</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="breakfast.html">Breakfast</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="dinner.html">Dinner</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="dessert.html">Dessert</a>
-      </li>
-    </ul>
-  </div>
-
-</nav>
 <div class="container">
 <br>
   <header style="font-size:1.75em; text-align: center">ABOUT US</header>
