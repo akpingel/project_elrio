70a42e33a6301518cc21a2abaf79b1d38d0d5d32
Alyssa Pingel <alyssa.pingel@my.simpson.edu>
2018-10-23 09:50:01 -0500

    changes made to php

---
+++ public_html/about.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+  <title>El Rio</title>
+
+    <!-- This says our CSS is compatible with phones, and phones
+         don't need to scale it strangely. -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Main CSS styles for Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Optional theme add-on -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/lux/bootstrap.min.css">
+   
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="index.html">El Rio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!--<div class="d-none d-md-block col-2"> -->
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Home </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="about.html">About<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="drinks.html">Drinks</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="breakfast.html">Breakfast</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dinner.html">Dinner</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dessert.html">Dessert</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
+<div class="container">
+<br>
+  <header style="font-size:1.75em; text-align: center">ABOUT US</header>
+  <h2 style="font-size:2em; text-align: center; text-decoration: underline">El Rio &centerdot; Mexican Restaurant</h2>
+  <br>
+  <br>
+  
+        <img class="center" src="img/flag.png" alt="Mexican Flag" />
+        <br>
+        <br>
+      <h4 class="mt-0">We have been the area's leading Mexican Restaurant for the past 20 years!</h4>
+      <p>- We serve our local area authentic Mexican food in a variety of ways.</p>
+      <p>- We offer outdoor seating with frequent live musical performances, dine-in, takeout, and more.</p>
+      <p>- Visit us for all your Mexican restaurant needs!</p>
+      <br>
+      
+      <br>
+      <br>
+     <div class="row">
+     <div class="col">
+      <div class="alert alert-light w-50" role="alert">
+      <h4>Contact Us!</h4></div>
+        <p>- Give us a call at 123-456-7890</p>
+        <p>-Or email us below </p>
+        <br>
+        <form>
+        <p> To: elrio@yahoo.com</p>
+        <div class="form-group">
+          <label for="exampleInputEmail1">From:</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+        
+        </div>
+       <div class="form-group">
+          <label for="exampleFormControlTextarea1">Type Email Here</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            </div>
+
+<br>
+<br>
+<div class="col">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<table class="table table-sm">
+  <thead class="thead-dark">
+    <tr>
+        
+        <th scope="col">Day</th>
+        <th scope="col">Hours</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+        <th scope="row">Monday - Thursday</th>
+        <td>8:00 AM - 10:00 PM</td>
+    </tr>
+    <tr>
+        <th scope="row">Friday - Saturday</th>
+        <td>7:00 AM - 11:00 PM</td>
+    </tr>
+    <tr>
+        <th scope="row">Sunday</th>
+        <td>9:00 AM - 9:00 PM</td>
+    </tr> 
+  </tbody>  
+</table>
+</div>
+</div>
+
+
+  <br>
+  <br>
+  <br>
+  
+
+<footer id="footer">
+&copy; 2018 Copyright &centerdot; El Rio Restaurant &centerdot; 012 Center Street, Indianola, Iowa, 50125 &centerdot; 212-345-6789
+</footer>
+</div>
+</body>
+</html>
---
+++ public_html/drinks.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+  <title>El Rio</title>
+
+    <!-- This says our CSS is compatible with phones, and phones
+         don't need to scale it strangely. -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Main CSS styles for Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <!-- Optional theme add-on -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/lux/bootstrap.min.css">
+
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="index.html">El Rio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.html">About</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="drinks.html">Drinks<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="breakfast.html">Breakfast</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dinner.html">Dinner</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="dessert.html">Dessert</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<header><h1 class="btn btn-warning">Drinks</h1></header>
+<br>
+<div class="container">
+<h3>Weekly Specials</h3>
+<p><b>Margarita Mondays: </b>Any size for only $3.50!</p>
+<p><b>Tequila Tuesdays: </b>Get a shot of tequila for $1.00!</p>
+<p><b>Wednesday Drink Deals</b>$3.00 Margaritas and $2.00 Draft Beer!</p>
+<p><b>Weekend Specials: </b>Jumbo Margaritas for $6.00 and $2.00 Draft Beer from 3pm to 6pm!</p>
+<br>
+<div class="border border-dark p-3">
+<h3>Margaritas <img src="img/margarita.png" alt="Margarita"/></h3>
+<p>Lime, Mango, Peach, Raspberry, Strawberry</p>
+<table>
+    <tr>
+        <th>Small</th>
+        <th>Medium</th>
+        <th>Large</th>
+    </tr>
+    <tbody>
+    <tr>
+        <td>&dollar;3.50</td>
+        <td>&dollar;4.50</td>
+        <td>&dollar;5.50</td>
+    </tr>
+    </tbody>
+</table>
+</div>
+<br>
+<div class="border border-dark p-3">
+    <h3>Daiquiris <img src="img/daiquiri.png" alt="Daiquiri" /></h3>
+    <h4 style="font-size: 1em"><em> &lpar;Pricing same as Margaritas&rpar;</em></h4>
+<p>Strawberry, Cherry, Mango</p>
+</div>
+<br>
+<div class="border border-dark p-3">
+    <h3>Imported Beer &dollar;4</h3>
+    <p>Corona, Modelo Especial, Dos Equis, Heineken</p>
+</div>
+<br>
+<div class="border border-dark p-3">
+    <h3>Domestic Beer &dollar;4 <img src="img/beer.png" alt="Bud Light" /></h3>
+    <p>Bud Light, Coors Light, Michelob Ultra</p>
+</div>  
+<br>
+<div class="border border-dark p-3">
+    <h3>Shots &dollar;2 <img src="img/tequila_shot.png" alt="Tequila Shot" /></h3>
+    <p>Tequila, Fire Ball, Jolly Rancher, Captain Morgan, Vodka</p>
+</div>
+<br>
+<div class="border border-dark p-3">
+    <h3>Soft Drinks &dollar;1.50</h3>
+    <p>Pepsi, Diet Pepsi, Mt. Dew, Cherry Pepsi, Sprite</P>
+    <p>Water, Milk, Ice Tea</P>
+    <p><em>Free refills on all soft drinks</em></p>
+</div>
+<br>
+<footer>
+&copy; 2018 Copyright &centerdot; El Rio Restaurant &centerdot; 012 Center Street, Indianola, Iowa, 50125 &centerdot; 212-345-6789
+</footer>
+</div>
+</body>
+</html>
