e39dc2878ff7ee5809b59c27cef1182bbeca7ec1
Alyssa Pingel <alyssa.pingel@my.simpson.edu>
2018-10-30 14:59:39 -0500

    changes to about.php

--- public_html/about.php
+++ public_html/about.php
@@ -104,9 +104,7 @@
   <br>
   
 
-<footer id="footer">
-&copy; 2018 Copyright &centerdot; El Rio Restaurant &centerdot; 012 Center Street, Indianola, Iowa, 50125 &centerdot; 212-345-6789
-</footer>
+<?php include '../resources/footer.php'; ?>
 </div>
 </body>
 </html>
